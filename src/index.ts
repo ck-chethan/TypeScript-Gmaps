7292d48eda4ea66c5fcb83654924a058d82123fe
chethanck <chethanckm66@gmail.com>
2022-01-11 08:57:30 +0530

    Google Custom maps

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// import { User } from './User';
+// import { Company } from './Company';
+
+// const usr = new User();
+// const company = new Company();
+// console.log(usr);
+// console.log(company);
+
+import { CustomMap } from './CustomMap';
+
+const customMap = new CustomMap('map');
